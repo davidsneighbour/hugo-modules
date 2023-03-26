9e495341104d748ded0bb330aca8f92895919f92
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-03-26 22:24:08 +0700

    chore(release): v0.0.6
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- static/sw-dev/service-worker.js
+++ static/sw-dev/service-worker.js
@@ -78,7 +78,7 @@ define(['./workbox-0b008d8b'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "main.8ea9cc15751fa110995e.js",
+    "url": "main.2d0074aeba261f610c59.js",
     "revision": null
   }], {});
 
