8e6950fe9f073914550d3f94ad33c0d88cffd8eb
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-03-27 19:57:37 +0700

    build: webpack configuration rework

---
+++ assets/js/blockify.js
@@ -0,0 +1,4 @@
+import LiteYTEmbed from '../libs/lite-youtube-embed/lite-yt-embed';
+
+// Register custom element
+customElements.define('lite-youtube', LiteYTEmbed);
--- webpack.js
+++ webpack.js
@@ -1,4 +1,6 @@
 const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { merge } = require("webpack-merge");
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
 class FileListPlugin {
@@ -26,7 +28,7 @@ class FileListPlugin {
   }
 }
 
-module.exports = {
+common = {
   target: 'webworker',
   entry: {
     main: path.join(__dirname, 'assets/js', 'service-worker.js'),
@@ -50,3 +52,45 @@ module.exports = {
     }),
   ]
 };
+
+const webworkerDevConfig = {
+  mode: "development",
+  "devtool": "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, 'static/sw-dev'),
+    filename: '[name].[fullhash].js',
+    clean: true,
+  },
+};
+
+const webworkerDistConfig = {
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, 'static/sw-prod'),
+    filename: '[name].[fullhash].js',
+    clean: true,
+  },
+};
+
+const scriptDistConfig = {
+  target: "web",
+  mode: "production",
+  entry: {
+    main: path.join(__dirname, 'assets/js', 'blockify.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'assets/dist/js'),
+    //filename: '[name].[fullhash].js',
+    filename: '[name].js',
+    clean: true,
+  },
+  // plugins: [
+  //   new FileListPlugin(),
+  // ]
+};
+
+module.exports = [
+  merge(scriptDistConfig),
+  merge(common, webworkerDistConfig),
+  merge(common, webworkerDevConfig)
+];
