e4cceb2d6246d54f864677c4407065ddac190ef3
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-07-30 21:11:10 +0700

    build: update release hooks system

--- modules/renderhooks/bin/release-hook-postrelease.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# declare path to this script
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
-
-# send all tags to remote
-git push --follow-tags origin main
-git push --tags
