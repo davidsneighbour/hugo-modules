ee6c754aac308d816899d0e066fb779ff75b1857
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-10-31 21:19:36 +0700

    feat: copy button for renderhook

---
+++ modules/renderhooks/assets/js/plugins/clipboard.js
@@ -0,0 +1,19 @@
+import ClipboardJS from '../libs/clipboard/clipboard';
+
+const clipboard = new ClipboardJS('.btn-clipboard');
+
+window.addEventListener('load', () => {
+  document.querySelectorAll('.btn-clipboard').forEach(btn => {
+    btn.classList.remove('invisible');
+  })
+});
+
+clipboard.on('success', event => {
+
+  setTimeout(() => {
+    event.clearSelection()
+  }, 500);
+
+});
+
+clipboard.on('error', event => { });
