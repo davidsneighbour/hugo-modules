4fbf8dc9528dd8734c8240978fbe78cbf88b12f1
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-11-16 18:16:05 +0700

    build(feat): update and optimize release scripts

--- bin/release/system/update.mjs
+++ bin/release/system/update.mjs
@@ -2,6 +2,7 @@
 import fetch from 'node-fetch';
 import 'dotenv/config';
 import fs from 'fs';
+import path from 'path';
 
 const { RELEASE_GIST } = process.env;
 const RELEASE_GIST_URL = `https://api.github.com/gists/${RELEASE_GIST}`;
@@ -15,20 +16,27 @@ fetch(RELEASE_GIST_URL)
 
     // create release hooks
     for (const fileName in gist.files) {
+      if (path.extname(fileName) === '.md') {
+        continue;
+      }
       const fileContent = gist.files[fileName].content;
-      // const fileNameFull = 'bin/release/' + fileName;
-      // fs.writeFileSync(fileNameFull, fileContent);
-      // console.log(`Updated ${fileNameFull}`);
 
       // create the hook in each module
       const dir = fs.opendirSync('modules');
       let dirent;
-      while ((dirent = dir.readSync()) !== null) {
-        if (dirent.name !== '.gitignore') {
-          const fileNameFull1 = 'modules/' + dirent.name + '/bin/release/' + fileName;
-          fs.writeFileSync(fileNameFull1, fileContent);
-          console.log(`Updated ${fileNameFull1}`);
+      try {
+        while ((dirent = dir.readSync()) !== null) {
+          if (dirent.name !== '.gitignore') {
+            const fileNameFull1 = `modules/${dirent.name}/bin/release/${fileName}`;
+            fs.writeFileSync(fileNameFull1, fileContent);
+            console.log(`Updated ${fileNameFull1}`);
+          }
         }
+      } finally {
+        dir.closeSync(); // Explicitly close the directory handle
       }
     }
+  })
+  .catch((error) => {
+    console.error('Error fetching or processing the gist:', error);
   });
