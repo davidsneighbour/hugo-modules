49da318de92cce3494b97112a4cb04b36b3eedf5
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-04-04 17:17:56 +0700

    fix: move clipboard copy button into tab bar

--- modules/renderhooks/assets/js/plugins/clipboard.js
+++ modules/renderhooks/assets/js/plugins/clipboard.js
@@ -2,11 +2,11 @@ import ClipboardJS from '../libs/clipboard/clipboard';
 
 const clipboard = new ClipboardJS('.btn-clipboard');
 
-window.addEventListener('load', () => {
-  document.querySelectorAll('.btn-clipboard').forEach(btn => {
-    btn.classList.remove('invisible');
-  })
-});
+// window.addEventListener('load', () => {
+//   document.querySelectorAll('.btn-clipboard').forEach(btn => {
+//     btn.classList.remove('invisible');
+//   })
+// });
 
 clipboard.on('success', event => {
 
