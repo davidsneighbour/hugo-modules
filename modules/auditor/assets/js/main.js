162d4f4ee8700be117dec87168ce8b96f18f7556
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-09-09 15:00:34 +0700

    fix(auditor): disable performance observer debugging
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- modules/auditor/assets/js/main.js
+++ modules/auditor/assets/js/main.js
@@ -1,10 +1,10 @@
-const observer = new PerformanceObserver((list) => {
-  console.log(list.getEntries());
-});
+// const observer = new PerformanceObserver((list) => {
+//   console.log(list.getEntries());
+// });
 
-observer.observe({ type: "long-animation-frame", buffered: true });
+// observer.observe({ type: "long-animation-frame", buffered: true });
 
-const loafs = performance.getEntriesByType("long-animation-frame");
-console.log(loafs);
+// const loafs = performance.getEntriesByType("long-animation-frame");
+// console.log(loafs);
 
-// https://developer.mozilla.org/en-US/docs/Web/API/Performance_API/Long_animation_frame_timing
+// // https://developer.mozilla.org/en-US/docs/Web/API/Performance_API/Long_animation_frame_timing
